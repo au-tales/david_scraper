befca99404005acd9a2795b9e0d1106e0cbef83b
Muhammad Umer Javed <muhammadumerjaved44@gmail.com>
2022-09-22 14:47:41 +0500

    add libs and ip

--- main.py
+++ main.py
@@ -84,4 +84,4 @@ def get_all_records():
     return {"data": data}
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", host="0.0.0.0", port=8060, reload=True)
+    uvicorn.run("main:app", host="127.0.0.1", port=8060, reload=True)
